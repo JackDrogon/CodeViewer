8ef9d3af82fdee195d4625b8f0590118032cfe71
Jack Drogon <jack.xsuperman@gmail.com>
2022-08-16 23:02:09 +0800

    Fix utils.remove_anon like leveldb::__anon008c292b0111 to leveldb for namespace name

--- code_viewer/utils.py
+++ code_viewer/utils.py
@@ -1,10 +1,17 @@
 # coding: utf-8
 
+from dataclasses import fields
+
 
 def remove_anon(name: str) -> str:
     # if scope split by ::, any one contains str startwiths "__anon", remove it
     # like leveldb::__anon2cdfda410111::PosixEnv => leveldb::PosixEnv
-    for field in name.split("::"):
-        if field.startswith("__anon"):
-            name = name.replace(f"{field}::", "")
+    for index, field in enumerate(name.split('::')):
+        if not field.startswith("__anon"):
+            continue
+
+        if index == 0:
+            name = name.replace(field, "")
+        else:
+            name = name.replace(f"::{field}", "")
     return name
