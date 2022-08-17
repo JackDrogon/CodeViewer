0ab12dfcb5be08a9856ccb3574990cecde31d7fc
Jack Drogon <jack.xsuperman@gmail.com>
2022-08-18 01:02:28 +0800

    Refactor SymbolTest split str test

--- tests/symbol_test.py
+++ tests/symbol_test.py
@@ -16,20 +16,21 @@ class SymbolTest(unittest.TestCase):
     test: create symbol from tag
     """
 
-    def test_create_symobol(self) -> None:
-        tag = {
-            "_type": "tag",
-            "name": "leveldb::VersionEdit::deleted_files_",
-            "path": "db/version_edit.h",
-            "pattern": "/^  DeletedFileSet deleted_files_;$/",
-            "language": "C++",
-            "typeref": "typename:DeletedFileSet",
-            "kind": "member",
-            "access": "private",
-            "scope": "leveldb::VersionEdit",
-            "scopeKind": "class"
-        }
-        symbol = Symbol(tag)
+    tag = {
+        "_type": "tag",
+        "name": "leveldb::VersionEdit::deleted_files_",
+        "path": "db/version_edit.h",
+        "pattern": "/^  DeletedFileSet deleted_files_;$/",
+        "language": "C++",
+        "typeref": "typename:DeletedFileSet",
+        "kind": "member",
+        "access": "private",
+        "scope": "leveldb::VersionEdit",
+        "scopeKind": "class"
+    }
+
+    def test_symobol_create(self) -> None:
+        symbol = Symbol(self.tag)
 
         # check all fields of symbol are set correctly
         self.assertEqual(symbol.name, "leveldb::VersionEdit::deleted_files_")
@@ -40,6 +41,10 @@ class SymbolTest(unittest.TestCase):
         self.assertEqual(
             str(symbol),
             'leveldb::VersionEdit::deleted_files_ db/version_edit.h:0')
+
+    def test_symbol_str(self) -> None:
+        symbol = Symbol(self.tag)
+
         buffer = Buffer()
         symbol.to_plantuml(buffer)
         self.assertEqual(
